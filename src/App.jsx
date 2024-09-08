715c45ef147d5058e752b9d651abb8662c0a255a
sahidDev09 <iamsahidofficial99@gmail.com>
2024-09-08 12:00:09 +0600

    setup all routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
 import Landing from "./pages/Landing";
 import Onboarding from "./pages/Onboarding";
+import Joblisting from "./pages/Job-listing";
+import Job from "./pages/Job";
+import PostJobs from "./pages/Post-Jobs";
+import SavedJobs from "./pages/Saved-jobs";
+import MyApplications from "./pages/My-applications";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +19,26 @@ const router = createBrowserRouter([
       {
         path: "/onboarding",
         element: <Onboarding />,
+      },
+      {
+        path: "/jobs",
+        element: <Joblisting />,
+      },
+      {
+        path: "/job/:id",
+        element: <Job />,
+      },
+      {
+        path: "/post-job",
+        element: <PostJobs />,
+      },
+      {
+        path: "/saved-jobs",
+        element: <SavedJobs />,
+      },
+      {
+        path: "/my-applications",
+        element: <MyApplications />,
       },
     ],
   },
