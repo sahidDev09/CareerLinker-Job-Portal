966272be13d969ce899dbcfa46aae311eba823a4
sahidDev09 <iamsahidofficial99@gmail.com>
2024-09-10 02:37:11 +0600

    connect supabase and clerk for authentication

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom";
-import { Button } from "./ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const Header = () => {
   return (
@@ -8,7 +13,14 @@ const Header = () => {
         <Link>
           <img src="./careerLinkerLogo.png" alt="" className=" h-20" />
         </Link>
-        <Button variant="outline">Login</Button>
+        {/* <Button variant="outline">Login</Button> */}
+
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </nav>
     </>
   );
